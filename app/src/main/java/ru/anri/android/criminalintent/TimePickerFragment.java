6c94643df8dcfdf872645c7d7a135e35706631e3
tnt2ultra <46776367+tnt2ultra@users.noreply.github.com>
2019-01-21 17:50:22 +0300

    восстановил

---
+++ app/src/main/java/ru/anri/android/criminalintent/TimePickerFragment.java
@@ -0,0 +1,96 @@
+package ru.anri.android.criminalintent;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class TimePickerFragment extends DialogFragment {
+    public static final String EXTRA_TIME = "ru.anri.android.criminalintent.time";
+    private static final String ARG_TIME = "time";
+
+    private TimePicker mTimePicker;
+    private int mHour;
+    private int mMinute;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Date time = (Date) getArguments().getSerializable(ARG_TIME);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(time);
+//        int year = calendar.get(Calendar.YEAR);
+//        int month = calendar.get(Calendar.MONTH);
+//        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        mHour = hour;
+        mMinute = minute;
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        mTimePicker = (TimePicker) v.findViewById(R.id.dialog_time_picker);
+//        mTimePicker.setHour(hour);
+//        mTimePicker.setMinute(minute);
+        mTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int pHour, int pMinute) {
+                mHour = pHour;
+                mMinute = pMinute;
+            }
+        });
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.time_picker_title)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                int year = mTimePicker.getYear();
+//                                int month = mTimePicker.getMonth();
+//                                int day = mTimePicker.getDayOfMonth();
+//                                int hour = mTimePicker.getHour();
+//                                int minute = mTimePicker.getMinute();
+//                                Date time1 = new GregorianCalendar(year, month, day).getTime();
+                                Date time1 = new Date();
+                                time1.setHours(mHour);
+                                time1.setMinutes(mMinute);
+                                sendResult(Activity.RESULT_OK, time1);
+                            }
+                        }
+                )
+                .create();
+    }
+
+    private void sendResult(int resultCode, Date time) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_TIME, time);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+    }
+
+    public static TimePickerFragment newInstance(Date time) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TIME, time);
+
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
